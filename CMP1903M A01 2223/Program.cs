aeacc39ae859c4228d3c32dee88a5a2da6335151
fn389 <fnajda389@gmail.com>
2023-03-06 12:48:29 +0000

    Added all functionality

--- CMP1903M A01 2223/Program.cs
+++ CMP1903M A01 2223/Program.cs
@@ -10,17 +10,6 @@ namespace CMP1903M_A01_2223
     {
         static void Main(string[] args)
         {
-            /*Pack pack = new Pack();
-            Card card = Pack.pack[0];
-            Console.WriteLine($"Start: Card: {card.Value} of {card.Suit}");
-
-            Pack.shuffleCardPack(1);
-            card = Pack.pack[0];
-            Console.WriteLine($"FisherYates: Card: {card.Value} of {card.Suit}");
-            Pack.shuffleCardPack(2);
-            card = Pack.pack[0];
-            Console.WriteLine($"RiffleShuffle Card: {card.Value} of {card.Suit}");
-            Console.ReadLine();*/
 
             Console.WriteLine("Running program");
             Testing test = new Testing();
